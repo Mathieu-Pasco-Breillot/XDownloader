d2d44a446d67427ddda7392b668ea20bac8773a7
Mathieu-Pasco-Breillot <mathieu.pasco@gmail.com>
2018-03-27 14:01:57 +0200

    Mapping du fichier de configuration avec des classes concrètes.

---
+++ XDownloader/Models/XConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XDownloader.Models
+{
+    public class XConfig
+    {
+        public List<Source> Sources { get; set; }
+    }
+
+    public class Source
+    {
+        public string Name { get; set; }
+        public string URL { get; set; }
+        public List<Show> Shows { get; set; }
+    }
+
+    public class Show
+    {
+        public string Name { get; set; }
+        public List<Season> Seasons { get; set; }
+    }
+
+    public class Season
+    {
+        public int Number { get; set; }
+        public List<Link> Links { get; set; }
+    }
+
+    public class Link
+    {
+        public string Source { get; set; }
+        public string Destination { get; set; }
+    }
+}
