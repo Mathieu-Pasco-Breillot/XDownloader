2ba1868b474a1a98481f78a9459944c0db053ea2
Mathieu-Pasco-Breillot <mathieu.pasco@gmail.com>
2018-03-27 14:01:10 +0200

    Ajout de la configuration de Log4Net et activation du module. Ajout du module Selenium.Chrome.WebDriver.

--- XDownloader/Program.cs
+++ XDownloader/Program.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -15,6 +18,8 @@ namespace XDownloader
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
