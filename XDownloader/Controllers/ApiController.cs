a6a08263809705aa15f76647749a1b33e94d3ec7
Mathieu-Pasco-Breillot <mathieu.pasco@gmail.com>
2018-04-10 14:46:30 +0200

    Ajout de 2 packages nuget afin de pouvoir faire usage de selenium au complet. Nouvelle méthode qui gère l'authentification sur l'hébergeur uptobox, le seul géré jusqu'à présent.

--- XDownloader/Controllers/ApiController.cs
+++ XDownloader/Controllers/ApiController.cs
@@ -1,9 +1,13 @@
 ﻿using HtmlAgilityPack;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Reflection;
 using XDownloader.Models;
 
 namespace XDownloader.Controllers
@@ -29,7 +33,40 @@ namespace XDownloader.Controllers
 
         #region Public Methods
 
-        [HttpPost("LinksFromHoster", Name = "Get_All_Links_From_Page")]
+        [HttpPost("LinksFromProtector", Name = "Get_Link_From_Protector_Page")]
+        public void GetLinkFromProtector([FromBody] string protecterURL)
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("headless");
+
+            using (var driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), options))
+            {
+                driver.Navigate().GoToUrl(protecterURL);
+                var button = driver.FindElement(By.ClassName("continuer"));
+                button.Click();
+                var link = driver.FindElement(By.PartialLinkText("uptobox.com")).Text;
+
+                string relativeLink = string.Empty;
+                Host host = xConfig.Hosts.First(h => h.Name == "uptobox");
+
+                // traitement du cas ou le lien est n'est pas en https
+                if (link.Contains("https"))
+                    relativeLink = link.Replace(host.URL, string.Empty);
+                else
+                    relativeLink = link.Replace(host.URL.Replace("https", "http"), string.Empty);
+                // authentication on uptobox.com
+                driver.Navigate().GoToUrl($"{host.URL}?op=login&referer={relativeLink}");
+                driver.FindElementByCssSelector("#login-form > input[name=login]").SendKeys(host.Login);
+                driver.FindElementByCssSelector("#login-form > input[name=password]").SendKeys(host.Password);
+                driver.FindElementByCssSelector("#login-form > div > button").Click();
+
+                //driver.Navigate().GoToUrl(link);
+                button = driver.FindElement(By.PartialLinkText("lancer votre téléchargement"));
+                string downloadLink = button.GetAttribute("href");
+            }
+        }
+
+        [HttpPost("LinksFromHoster", Name = "Get_All_Links_From_Hoster_Page")]
         public HttpStatusCode GetLinksFromHoster([FromBody] string hosterURL)
         {
             HtmlWeb web = new HtmlWeb();
@@ -54,7 +91,6 @@ namespace XDownloader.Controllers
                 return HttpStatusCode.Unauthorized;
             }
         }
-
         #endregion Public Methods
     }
 }
