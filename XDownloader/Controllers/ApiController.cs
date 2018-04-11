fa4240039416d376aa55ec8d339eed44994ea109
Mathieu PASCO-BREILLOT <mathieu.pasco@gmail.com>
2018-04-11 23:22:04 +0200

    Factorisation du code, éclatement en plusieurs méthodes, plusieurs sujets en travaux.

--- XDownloader/Controllers/ApiController.cs
+++ XDownloader/Controllers/ApiController.cs
@@ -44,50 +44,32 @@ namespace XDownloader.Controllers
 
             using (var driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), options))
             {
-                driver.Navigate().GoToUrl(protecterURL);
-                var button = driver.FindElement(By.ClassName("continuer"));
-                button.Click();
-                var link = driver.FindElement(By.PartialLinkText("uptobox.com")).Text;
-
+                string link = FindHostUnderProtector(protecterURL, driver);
                 string relativeLink = string.Empty;
-                Host host = xConfig.Hosts.First(h => h.Name == "uptobox");
 
+                Host host = xConfig.Hosts.First(h => h.Name == "uptobox");
                 // traitement du cas ou le lien est n'est pas en https
                 if (link.Contains("https"))
                     relativeLink = link.Replace(host.URL, string.Empty);
                 else
                     relativeLink = link.Replace(host.URL.Replace("https", "http"), string.Empty);
+
+                #region Host Authentication And Redirection
+
                 // authentication on uptobox.com
                 driver.Navigate().GoToUrl($"{host.URL}?op=login&referer={relativeLink}");
                 driver.FindElementByCssSelector("#login-form > input[name=login]").SendKeys(host.Login);
                 driver.FindElementByCssSelector("#login-form > input[name=password]").SendKeys(host.Password);
                 driver.FindElementByCssSelector("#login-form > div > button").Click();
 
-                //driver.Navigate().GoToUrl(link);
-                button = driver.FindElementByCssSelector("#dl > form > div > center > a");
-                string downloadLink = button.GetAttribute("href");                
+                #endregion
+
+                // On récupère le lien de téléchargement fourni par l'hébergeur
+                string downloadLink = driver.FindElementByCssSelector("#dl > form > div > center > a").GetAttribute("href");
                 int slashIndex = downloadLink.LastIndexOf('/');
+                // On supprime le '/' se trouvant au début de l'URL.
                 string fileName = downloadLink.Substring(slashIndex + 1, downloadLink.Length - slashIndex - 1);
-
-                WebClient wb = new WebClient();                
-
-                try
-                {
-                    wb.DownloadFileAsync(new Uri(downloadLink), $"D:\\Videos\\{fileName}");
-                }
-                catch (ArgumentNullException ane)
-                {
-                    return BadRequest($"l'URL suivante est peut-être incorrecte : {downloadLink} ?\n Ou le chemin d'enregistrement : {fileName} est inconnu ?\n Exception : {ane}"); 
-                }
-                catch (WebException we)
-                {
-                    return StatusCode((int)HttpStatusCode.InternalServerError, we);
-                }
-                catch (InvalidOperationException ioe)
-                {
-                    return StatusCode((int)HttpStatusCode.InternalServerError, ioe);
-                }
-                return Ok($"Le téléchargement du fichier {fileName} est en cours.");
+                return ExecuteDownload(downloadLink, fileName);
             }
         }
 
@@ -107,7 +89,7 @@ namespace XDownloader.Controllers
                     attributesLinks.Add(absoluteLink);
                 }
                 if (attributesLinks.Count > 0)
-                    return Ok();
+                    return Ok(attributesLinks);
                 else
                     return NotFound();
             }
@@ -118,5 +100,59 @@ namespace XDownloader.Controllers
         }
 
         #endregion Public Methods
+
+        private List<string> FilterUriList(List<HtmlAttribute> attributesLinks)
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("headless");
+
+            using (var driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), options))
+            {
+                List<string> uriList = new List<string>();
+                foreach (HtmlAttribute attribute in attributesLinks)
+                {
+                    string link = FindHostUnderProtector(attribute.Value, driver);
+                }
+                return uriList;
+            }
+        }
+
+        private static string FindHostUnderProtector(string protecterURL, ChromeDriver driver)
+        {
+            try
+            {
+                driver.Navigate().GoToUrl(protecterURL);
+                driver.FindElement(By.ClassName("continuer")).Click();
+                var link = driver.FindElementByPartialLinkText("uptobox.com").Text;
+                return link;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        private IActionResult ExecuteDownload(string downloadLink, string fileName)
+        {
+            WebClient wb = new WebClient();
+            try
+            {
+                // On lance le téléchargement du fichier de façon asynchrone.
+                wb.DownloadFileAsync(new Uri(downloadLink), $"D:\\Videos\\{fileName}");
+            }
+            catch (ArgumentNullException ane)
+            {
+                return BadRequest($"l'URL suivante est peut-être incorrecte : {downloadLink} ?\n Ou le chemin d'enregistrement : {fileName} est inconnu ?\n Exception : {ane}");
+            }
+            catch (WebException we)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, we);
+            }
+            catch (InvalidOperationException ioe)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ioe);
+            }
+            return Ok($"Le téléchargement du fichier {fileName} est en cours.");
+        }
     }
 }
