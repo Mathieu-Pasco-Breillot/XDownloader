9c60901a2b239992580497bc8ae070ff92fb29e5
Mathieu PASCO-BREILLOT <mathieu.pasco@gmail.com>
2018-04-15 18:51:33 +0200

    Ajout d'un model pour mapper la récption des paramètres des requêtes reçues.

---
+++ XDownloader/Models/RequestParams.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace XDownloader.Models
+{
+    public class RequestParameters
+    {
+        #region Public Properties
+
+        public List<string> Hosts { get; set; }
+        public string Url { get; set; }
+
+        #endregion Public Properties
+    }
+}
