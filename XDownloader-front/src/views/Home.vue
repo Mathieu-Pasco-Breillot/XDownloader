1a437cddf595773401b6c66643775dd945e87b7f
Alexandre Liscia <alexandre.liscia@gmail.com>
2018-04-30 23:07:08 +0200

    Mise en place du composant DownloadForm

--- XDownloader-front/src/views/Home.vue
+++ XDownloader-front/src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
     <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <DownloadForm/>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+  import { Component, Vue } from 'vue-property-decorator';
+  import DownloadForm from '@/components/DownloadForm.vue'; // @ is an alias to /src
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {}
+  @Component({
+    components: {
+      DownloadForm,
+    },
+  })
+  export default class Home extends Vue {
+  }
 </script>
