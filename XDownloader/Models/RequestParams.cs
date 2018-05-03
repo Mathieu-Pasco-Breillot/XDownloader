126f3989ef7f09d2bda3bd267b7b0509863eef81
Mathieu-Pasco-Breillot <mathieu.pasco@gmail.com>
2018-05-03 17:01:25 +0200

    #9 Ajout de commentaires sur chaque méthode de l'ApiController et sur l'ensemble du RequestParams.
    Cela permettra ultérieurement de générer la documentation automatique.

--- XDownloader/Models/RequestParams.cs
+++ XDownloader/Models/RequestParams.cs
@@ -2,15 +2,29 @@
 
 namespace XDownloader.Models
 {
+    /// <summary>
+    /// Model which represent the content of any request parameters send.
+    /// </summary>
     public class RequestParameters
     {
         #region Public Properties
 
+        /// <summary>
+        /// The list of hosts that should be handle by the scrapper.
+        /// </summary>
         public List<string> Hosts { get; set; }
+
+        /// <summary>
+        /// Any kind of url, it may be a protected url or a source url.
+        /// </summary>
         public string Url { get; set; }
 
         #endregion Public Properties
 
+        /// <summary>
+        /// Display the properties content.
+        /// </summary>
+        /// <returns>A string which contains the given parameters content.</returns>
         public override string ToString()
         {
             string hosts = string.Empty;
