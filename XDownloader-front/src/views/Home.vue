55d96775fcb2cab4dba39b0be1025b12e9797f8a
Alexandre Liscia <alexandre.liscia@gmail.com>
2018-05-13 23:56:20 +0200

    Première version de la page

--- XDownloader-front/src/views/Home.vue
+++ XDownloader-front/src/views/Home.vue
@@ -1,19 +1,43 @@
 <template>
   <div class="home">
     <img src="../assets/logo.png">
-    <DownloadForm/>
+
+    <div class="grid">
+      <div class="protector">
+        <label for="protector">Get link from protector&nbsp;:</label>
+        <input type="text" placeholder="protector link">
+      </div>
+      
+      <div class="source">
+        <label for="source">Get link from source&nbsp;:</label>
+        <input type="text" placeholder="protector link">
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-  import { Component, Vue } from 'vue-property-decorator';
-  import DownloadForm from '@/components/DownloadForm.vue'; // @ is an alias to /src
+import { Component, Vue } from 'vue-property-decorator';
+// import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
-  @Component({
-    components: {
-      DownloadForm,
-    },
-  })
-  export default class Home extends Vue {
-  }
+@Component({
+  components: {
+  },
+})
+export default class Home extends Vue {}
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 1px;
+  background-color: lightgrey;
+}
+
+.grid > div {
+  background-color: white;
+  padding: 1em;
+}
+</style>
