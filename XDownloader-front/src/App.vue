25db36e78262fd8440878986a894ad81fc86377d
Alexandre Liscia <alexandre.liscia@gmail.com>
2018-05-13 23:56:20 +0200

    Première version de la page

--- XDownloader-front/src/App.vue
+++ XDownloader-front/src/App.vue
@@ -1,12 +1,19 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-      |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <div id="nav">
+            <router-link to="/">Home</router-link>
+            |
+            <router-link to="/about">About</router-link>
+        </div>
+
+        <div id="left"></div>
+
+        <div id="content">
+            <router-view/>
+        </div>
+
+        <div id="right"></div>
     </div>
-    <router-view/>
-  </div>
 </template>
 
 <style>
@@ -16,9 +23,17 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  display: grid;
+  grid-template-areas:
+    "header header header header header"
+    "left content content content right"
+    "left footer footer footer footer";
+  grid-gap: 1em;
 }
+
 #nav {
   padding: 30px;
+  grid-area: header;
 }
 
 #nav a {
@@ -28,5 +43,14 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+#left {
+  grid-area: left;
+}
+
+#content {
+  grid-area: content;
+  justify-self: center;
 }
 </style>
