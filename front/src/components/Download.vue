f8a16ae772a2a6ab9464a7e76bc2638c3582715d
Alexandre Liscia <alexandre.liscia@gmail.com>
2018-06-08 14:34:16 +0200

    Initialisation correcte de vue avec TypeScript. Correction d'un variable non utilisée en C#.

---
+++ front/src/components/Download.vue
@@ -0,0 +1,45 @@
+<template>
+  <form>
+    <h1>XDownloader</h1>
+    <div class="grid">
+      <div>
+        <input type="text" placeholder="Get_Link_From_Protector_Page" v-model="protectorLink">
+        <button @click="getLinkFromProtectorPage">Valider</button>
+      </div>
+      <div></div>
+    </div>
+  </form>
+</template>
+
+<script lang="ts">
+  import Vue from "vue";
+
+  export default Vue.extend( {
+    name: "Download",
+    data: {
+      protectorLink: "",
+    },
+    methods: {
+      getLinkFromProtectorPage(): void {
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", "http://localhost:")
+      }
+    }
+  } );
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h3 {
+    margin: 40px 0 0;
+  }
+
+  a {
+    color: #42b983;
+  }
+
+  .grid {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+</style>
---
+++ front/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img src="../assets/logo.png">
+    <Download/>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue      from "vue";
+  import Download from "@/components/Download.vue"; // @ is an alias to /src
+
+  export default Vue.extend( {
+    name      : "home",
+    components: {
+      Download,
+    },
+  } );
+</script>
