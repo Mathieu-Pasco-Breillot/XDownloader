86558fe806366d61162fe3d4dc6f67bed4e264d6
Alexandre Liscia <alexandre.liscia@gmail.com>
2018-06-16 00:08:28 +0200

    Ajout de Vuetify pour le material design. Look dark. Utilisation d'axios pr les requêtes au back.

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,10 +1,15 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
+import Vue     from 'vue';
+import App     from './App.vue';
+import router  from './router';
+import store   from './store';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
 
 Vue.config.productionTip = false;
+Vue.use( Vuetify );
 
-new Vue({
+new Vue( {
   router,
-  render: (h) => h(App),
-}).$mount('#app');
+  store,
+  render: (h) => h( App ),
+} ).$mount( '#app' );
--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
     <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Download/>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+  import { Component, Vue } from "vue-property-decorator";
+  import Download           from "@/components/Download.vue"; // @ is an alias to /src
 
-export default Vue.extend({
-  name: 'home',
-  components: {
-    HelloWorld,
-  },
-});
+  @Component( {
+    components: {
+      Download,
+    },
+  } )
+  export default class Home extends Vue {
+  }
 </script>
